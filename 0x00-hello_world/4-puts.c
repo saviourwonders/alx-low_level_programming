679b4141b6e298123f720aab147d95e597d49323
saviourwonders <saviourwonders@gmail.com>
2022-12-08 18:57:46 -0800

    Betty Added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *   * Return: Always 0                                                                                                                     
  *    */                                                                                                                                     
 int main(void)                                                                                                                          
-{                                                                                                                                       
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-			        return (0);
+{     
+	puts("\"Programming is like building a multi lingual puzzle);                                                            
+	return (0);
 }
