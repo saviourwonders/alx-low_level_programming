036a1b5ed8677a67203efa1564490fd7c9de7805
saviourwonders <saviourwonders@gmail.com>
2022-12-28 18:50:53 -0800

    correction made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry
  *
- * Return: Always 0 
- */ 
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
