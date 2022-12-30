23f1a74c0762bcadb2da03b5f4ef18e91f03abf8
saviourwonders <saviourwonders@gmail.com>
2022-12-29 23:44:36 -0800

    task 5 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print small letters except q & e
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
